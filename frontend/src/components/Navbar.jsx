aca3bcd4b2e4146af114ddc3f034b59a58e6202d
ManishaD788 <manishadwivedi56@gmail.com>
2026-07-21 22:28:55 +0530

    Create Navbar and Home components

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>Sentinel AI</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>Prompt Detector</li>
+        <li>Phishing Detector</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Sentinel AI</h1>
+      <p>AI Powered Cybersecurity Platform</p>
+    </div>
+  );
+}
+
+export default Home;
